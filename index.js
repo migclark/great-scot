e46d404b03218138df0b618b0cd90de816dee8d7
Miguel Clark <migclark@Miguels-MacBook-Air.local>
2015-09-22 21:44:23 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/category', function(request, response) {
+  response.render('pages/category');
+});
+
+app.get('/signup', function(request, response) {
+  response.render('pages/signup');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
